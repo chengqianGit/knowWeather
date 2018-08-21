c75096dc1a04aa35509328b72da81501ae60427b
cheng <1114638794@qq.com>
2018-08-21 21:35:28 +0800

    加入显示天气信息功能

--- app/src/main/java/com/knowweather/android/MainActivity.java
+++ app/src/main/java/com/knowweather/android/MainActivity.java
@@ -1,5 +1,8 @@
 package com.knowweather.android;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //判断是否有缓存数据,如果有缓存，则说明之前请求过数据了，没必要让用户再次选择数据
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getString("weather",null)!=null){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/knowweather/android/util/Utility.java
+++ app/src/main/java/com/knowweather/android/util/Utility.java
@@ -2,9 +2,11 @@ package com.knowweather.android.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.knowweather.android.db.City;
 import com.knowweather.android.db.County;
 import com.knowweather.android.db.Province;
+import com.knowweather.android.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -75,6 +77,20 @@ public class Utility {
             }
         }
         return false;
+    }
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
